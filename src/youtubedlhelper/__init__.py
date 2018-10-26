bf48b2e0ff9e83fce4f64090f3c570857438c194
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2018-10-26 08:20:22 +0000

    Add ability to specify download directory, and bump version.

--- src/youtubedlhelper/__init__.py
+++ src/youtubedlhelper/__init__.py
@@ -2,9 +2,10 @@
 
 from __future__ import print_function
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 import os
+import pickle
 import shlex
 import subprocess
 import sys
@@ -15,6 +16,10 @@ gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
 from gi.repository import Gdk
 from gi.repository import Gtk
+
+
+cfgdir = os.path.expanduser("~/.config/youtube-dl-helper")
+cfgfile = os.path.join(cfgdir, "config.pickle")
 
 
 def find_file(name):
@@ -29,10 +34,11 @@ def find_file(name):
     raise KeyError(name)
 
 
-def received(w, drag_context, x, y, data, info, time):
+def received(fcdb, w, drag_context, x, y, data, info, time):
     uris = data.get_uris() + data.get_text().replace("\n", " ").split()
     if not uris:
         return
+    destdir = fcdb.get_filename()
     cmd = ['youtube-dl', '--'] + uris
     cmd = ' '.join(shlex.quote(x) for x in cmd)
     cmd += ' && exit || { ret=$? ; >&2 echo There were errors.  Hit ENTER or close this window. ; read ; exit $ret ; }'
@@ -43,28 +49,57 @@ def received(w, drag_context, x, y, data, info, time):
         if ret != 0:
             print("The GNOME terminal process running youtube-dl terminated with status code %s" % ret, file=sys.stderr)
 
-    p = subprocess.Popen(command)
+    p = subprocess.Popen(command, cwd=destdir)
     t = threading.Thread(target=reap, args=(p,))
     t.setDaemon(True)
     t.start()
 
 
+def load_config():
+    global cfgfile
+    try:
+        return pickle.load(open(cfgfile, "rb"))
+    except Exception:
+        return {}
+
+
+def save_config(cfg):
+    global cfgdir
+    global cfgfile
+    if not os.path.isdir(cfgdir): os.makedirs(cfgdir)
+    pickle.dump(cfg, open(cfgfile, "wb"))
+
+
 def main():
+    cfg = load_config()
+    download_dir = cfg.get("download_dir", os.path.expanduser("~"))
+
     builder = Gtk.Builder()
     builder.add_from_file(find_file('youtube-dl-helper/youtube-dl-helper.glade'))
 
     main = builder.get_object('main')
     logo = builder.get_object('logo')
     logo.set_from_file(find_file('pixmaps/youtube-dl-helper.png'))
-    label = builder.get_object('vbox')
+    vbox = builder.get_object('vbox')
+    download_dir_fcdb = builder.get_object('download_dir')
+    download_dir_fcdb.set_filename(download_dir)
 
-    for w in [label]:
+    received_with_fcdb = lambda *a, **kw: received(download_dir_fcdb, *a, **kw)
+
+    for w in [vbox]:
         w.drag_dest_set(Gtk.DestDefaults.ALL, [], Gdk.DragAction.COPY)
         w.drag_dest_add_text_targets()
         w.drag_dest_add_uri_targets()
-        w.connect('drag-data-received', received)
+        w.connect('drag-data-received', received_with_fcdb)
 
-    main.connect("destroy", Gtk.main_quit)
+    def quit(*unused_a, **unused_kw):
+        cfg["download_dir"] = download_dir_fcdb.get_filename()
+        if cfg["download_dir"] == os.path.expanduser("~"):
+            del cfg["download_dir"]
+        save_config(cfg)
+        Gtk.main_quit()
+
+    main.connect("destroy", quit)
     main.show_all()
     Gtk.main()
 
